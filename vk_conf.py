570112df8f4bd265cfcac60c7e4917f87aa38d6f
Sergey Polikarpov <polikarpov1993sv@gmail.com>
2023-01-29 16:31:55 +0400

    1111

--- vk_conf.py
+++ vk_conf.py
@@ -1,5 +1,5 @@
 V = ''
 
-group_token = 'vk1.a.cvhouLQMz-G60CimzF3KqyDTJqTcPdKRg3iggiqokjnr6I_UcuADMpVpc8YreqhCkwWQ0WdTE2vH4ZK06-Xu_t6m3gUHg95ZREzYEBvkDMewlBduGZ3NPl2S8b3UXSmtK1wuorFYifFlFAr5SXlzcGHbELLXu8A3_OUNPtKjZw0fbutZgfmkRo8DLvKoOIYTJi9g_1Q2zJu7BYsmSSa09g'
+group_token = ''
 
 user_token = ''
